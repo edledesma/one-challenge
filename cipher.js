e13e2c111961fe1cd7a8c75e2769eb12f69bf583
edledesma <100038680+edledesma@users.noreply.github.com>
2024-02-06 20:25:06 -0300

    Functional cipher

--- cipher.js
+++ cipher.js
@@ -2,59 +2,48 @@ let secretMsg = document.querySelector('#messageContent')
 let secretMsgResult = document.querySelector('#messageResult')
 let msgArray = [];
 let returnString = "";
+let tempString = "";
 
-
+// Funcion que encripta los mensajes
 
 function encryptMsg(){
-    msgArray = secretMsg.value.split("");
-    for (let i = 0; i < msgArray.length; i++){
+    msgArray = secretMsg.value.split(""); // Convierte el mensaje a un arreglo separado por letras
+    for (let i = 0; i < msgArray.length; i++){ //Revisa cada letra del arreglo, si es una vocal su valor se reemplaza
         console.log(msgArray[i]);
-        switch (msgArray[i]){
-            case 'e':
+        if(msgArray[i] == 'e'){
                 msgArray[i] = "enter";
-                break;
-            case 'i':
-                msgArray[i] = "imes";
-                break;
-            case 'a':
-                msgArray[i] = "ai";
-                break;
-            case 'o':
-                msgArray[i] = "ober";
-                break;
-            case 'u':
-                msgArray[i] = "ufat";
-                break;
+        }else if(msgArray[i] == 'i'){
+            msgArray[i] = "imes";
+        }else if(msgArray[i] == 'a'){
+            msgArray[i] = "ai";
+        }else if(msgArray[i] == 'o'){
+            msgArray[i] = "ober";
+        }else if(msgArray[i] == "u"){
+            msgArray[i] = "ufat";
         }
         console.log(msgArray[i]);
     }
-    returnString = msgArray.toString(); 
-    returnString = returnString.replaceAll(',',"")
+    returnString = msgArray.toString();  // Retorna el arreglo a una cadena
+    returnString = returnString.replaceAll(',',"") //Remueve las comas del arreglo
     secretMsgResult.value = returnString;
 }
 
+// Funcion que decripta los mensajes
+
 function decryptMsg(){
-    msgArray = secretMsg.value.split("");
-    msgArray = msgArray.replace('enter','e')
-    msgArray = msgArray.replace('imes','i')
-    msgArray = msgArray.replace('ai','a')
-    msgArray = msgArray.replace('ober','o')
-    msgArray = msgArray.replace('ufat','u')
-    secretMsgResult.value = msgArray
+    tempString = secretMsg.value
+    while (tempString.includes("enter")||tempString.includes("imes")||tempString.includes("ai")||tempString.includes("ober")||tempString.includes("ufat")){
+        tempString = tempString.replace("enter", "e")
+        tempString = tempString.replace("imes", "i")
+        tempString = tempString.replace("ai", "a")
+        tempString = tempString.replace("ober", "o")
+        tempString = tempString.replace("ufat", "u")
+        console.log(tempString)
+    }
+    secretMsgResult.value = tempString;
 }
+// Funcion que copia el resultado al portapapeles
 
 function copyMsg(){
     navigator.clipboard.writeText(secretMsgResult.value);
 }
-
-/* La letra "i" es convertida para "imes"
-La letra "a" es convertida para "ai"
-La letra "o" es convertida para "ober"
-La letra "u" es convertida para "ufat"
-enter ai ober ufat imes 
-
-HOLA
-
-Hoberlai
-
-*/
