525e29dd21022c94245d6c54e8637c57833274fc
edledesma <100038680+edledesma@users.noreply.github.com>
2024-02-06 21:09:10 -0300

    design phase

--- cipher.js
+++ cipher.js
@@ -1,49 +1,62 @@
-let secretMsg = document.querySelector('#messageContent')
-let secretMsgResult = document.querySelector('#messageResult')
+const secretMsg = document.querySelector('#messageContent')
+const secretMsgResult = document.querySelector('#messageResult')
+const regex = /[^a-z0-9\s]/i; //Expresion regular que contiene los caracteres aceptados
 let msgArray = [];
 let returnString = "";
-let tempString = "";
 
 // Funcion que encripta los mensajes
 
 function encryptMsg(){
-    msgArray = secretMsg.value.split(""); // Convierte el mensaje a un arreglo separado por letras
-    for (let i = 0; i < msgArray.length; i++){ //Revisa cada letra del arreglo, si es una vocal su valor se reemplaza
-        console.log(msgArray[i]);
-        if(msgArray[i] == 'e'){
+    if (checkChar(secretMsg.value) == true){
+        secretMsgResult.value = "Caracter invalido detectado"
+    }else{
+        msgArray = secretMsg.value.toLowerCase().split(""); // Convierte el mensaje a un arreglo separado por letras
+        for (let i = 0; i < msgArray.length; i++){ //Revisa cada letra del arreglo, si es una vocal su valor se reemplaza
+            console.log(msgArray[i]);
+            if(msgArray[i] == 'e'){
                 msgArray[i] = "enter";
-        }else if(msgArray[i] == 'i'){
-            msgArray[i] = "imes";
-        }else if(msgArray[i] == 'a'){
-            msgArray[i] = "ai";
-        }else if(msgArray[i] == 'o'){
-            msgArray[i] = "ober";
-        }else if(msgArray[i] == "u"){
-            msgArray[i] = "ufat";
-        }
+            }else if(msgArray[i] == 'i'){
+                msgArray[i] = "imes";
+            }else if(msgArray[i] == 'a'){
+                msgArray[i] = "ai";
+            }else if(msgArray[i] == 'o'){
+                msgArray[i] = "ober";
+            }else if(msgArray[i] == "u"){
+                msgArray[i] = "ufat";
+            }
         console.log(msgArray[i]);
+        }
+        returnString = msgArray.toString();  // Retorna el arreglo a una cadena
+        returnString = returnString.replaceAll(',',"") //Remueve las comas del arreglo
+        secretMsgResult.value = returnString;
     }
-    returnString = msgArray.toString();  // Retorna el arreglo a una cadena
-    returnString = returnString.replaceAll(',',"") //Remueve las comas del arreglo
-    secretMsgResult.value = returnString;
 }
 
 // Funcion que decripta los mensajes
 
 function decryptMsg(){
-    tempString = secretMsg.value
-    while (tempString.includes("enter")||tempString.includes("imes")||tempString.includes("ai")||tempString.includes("ober")||tempString.includes("ufat")){
-        tempString = tempString.replace("enter", "e")
-        tempString = tempString.replace("imes", "i")
-        tempString = tempString.replace("ai", "a")
-        tempString = tempString.replace("ober", "o")
-        tempString = tempString.replace("ufat", "u")
-        console.log(tempString)
+    if (checkChar(secretMsg.value) == true){
+        secretMsgResult.value = "Caracter invalido detectado"
+    }else{
+        returnString = secretMsg.value.toLowerCase()
+        while (returnString.includes("enter")||returnString.includes("imes")||returnString.includes("ai")||returnString.includes("ober")||returnString.includes("ufat")){
+            returnString = returnString.replace("enter", "e")
+            returnString = returnString.replace("imes", "i")
+            returnString = returnString.replace("ai", "a")
+            returnString = returnString.replace("ober", "o")
+            returnString = returnString.replace("ufat", "u")
+        }
+        secretMsgResult.value = returnString;
     }
-    secretMsgResult.value = tempString;
 }
 // Funcion que copia el resultado al portapapeles
 
 function copyMsg(){
     navigator.clipboard.writeText(secretMsgResult.value);
+}
+
+// Funcion que controla si existen caracteres especiales o tildes
+
+function checkChar(message){
+    return regex.test(message)
 }
